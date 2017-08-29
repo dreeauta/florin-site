32a8be1a1e2dbfac535b405fe297975a847a95e5
Andreea Uta <dreea.9965@gmail.com>
2017-08-28 21:45:33 -0400

    site setup

---
+++ frontend/src/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as Redux from 'redux';
+import * as ReactRedux from 'react-redux';
+import ReduxThunk from 'redux-thunk';
+import { Router, Route, Link, IndexLink, IndexRoute, hashHistory } from 'react-router';
+
+import './index.css';
+
+import productsContainer from './products/products';
+import productsReducer from './products/products.reducer';
+import contactContainer from './contact/contact';
+import contactReducer from './contact/contact.reducer';
+
+const reducer = Redux.combineReducers({
+  products: productsReducer,
+  contact: contactReducer
+})
+
+const store = Redux.createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  Redux.compose(Redux.applyMiddleware(ReduxThunk))
+)
+
+class AppLayout extends React.Component {
+  render() {
+    return (
+      <div className="appLayout"> 
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
+const AppLayoutContainer = ReactRedux.connect(
+  state => state
+)(AppLayout);
+
+ReactDOM.render(
+  <ReactRedux.Provider store={store}>
+    <Router history={hashHistory}>
+      <Route path="/" component={AppLayoutContainer}>
+      <IndexRoute component={AppLayoutContainer}/>
+      <Route path="/products" component={productsContainer}/>
+      <Route path="contact" component="{contactcontainer}"/>
+      
+      </Route>
+    </Router>
+  </ReactRedux.Provider>,
+ document.getElementById('root')
+);
